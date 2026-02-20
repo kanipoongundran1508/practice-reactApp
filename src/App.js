132fc3993ae4f338505438415641e69c04d32b97
kanipoongundran1508@gmail.com <your.email@example.com>
2026-02-20 19:31:53 +0530

    Home page changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
     <Routes>
-      <Route path="/home" element={<Home/>} />
+      <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="blog" element={<Blog/>}/>
       <Route path='/product' element={<Dashboard/>}> 
